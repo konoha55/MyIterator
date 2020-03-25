6e2ee62b19bd27eb3e25249b3aff371d41c51086
konoha55 <hiroki.ideriha12@gmail.com>
2020-03-25 14:01:40 +0900

    MyIteratorプロジェクト初期コミット
    デザインパターンMyIteratorプロジェクト初期コミット

---
+++ src/iterator/BookShelf.java
@@ -0,0 +1,27 @@
+package iterator;
+
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+
+	private ArrayList<Book> books;
+
+	public BookShelf (int initialsize) {
+		this.books = new ArrayList<Book>(initialsize);
+	}
+	public Book getBookAt (int index) {
+		return books.get(index);
+	}
+	public void appendBook(Book book) {
+		this.books.add(book);
+	}
+	public int getLength() {
+		return books.size();
+	}
+
+	@Override
+	public Iterator iterator() {
+		return new BookShelfIterator(this);
+	}
+
+}
---
+++ src/iterator/Main.java
@@ -0,0 +1,23 @@
+package iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BookShelf bookShelf = new BookShelf(4);
+
+		bookShelf.appendBook(new Book("Around the World in 80Days"));
+		bookShelf.appendBook(new Book("Bible"));
+		bookShelf.appendBook(new Book("Cin"));
+		bookShelf.appendBook(new Book("Daddy"));
+		bookShelf.appendBook(new Book("Daddy2"));
+
+		Iterator it = bookShelf.iterator();
+
+		while (it.hasNext()) {
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+
+	}
+
+}
